4145177d876e6c05bbf5db55792b7b12f646ddde
Zakarum <zakarumych@ya.ru>
2021-08-03 21:20:14 +0300

    Cleanup

--- import/src/lib.rs
+++ import/src/lib.rs
@@ -7,12 +7,12 @@ mod material;
 mod sampler;
 mod sprite_sheet;
 
-pub use goods_treasury_import::ffi::{
+pub use goods_treasury_import_ffi::{
     treasury_importer_alloc, treasury_importer_dealloc, treasury_importer_import_trampoline,
     treasury_importer_name_source_native_trampoline,
 };
 
-goods_treasury_import::generate_imports_and_exports! {
+goods_treasury_import_ffi::generate_imports_and_exports! {
     &aseprite::SpriteSheetImporter,
     &image::ImageImporter,
     &sprite_sheet::SpriteSheetEnrich,
