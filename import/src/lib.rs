ae62b427e64f2e7420924d7d48c53beaac596151
Zakarum <zakarumych@ya.ru>
2022-01-21 01:24:24 +0300

    Assets cleanup

--- import/src/lib.rs
+++ import/src/lib.rs
@@ -1,24 +1,10 @@
-#![cfg(target_os = "wasi")]
-
-mod aseprite;
-mod gltf;
+// mod aseprite;
+// mod gltf;
 mod image;
-mod material;
-mod sampler;
-mod sprite_sheet;
+// mod material;
+// mod sampler;
+// mod sprite_sheet;
 
-pub use goods_treasury_import_ffi::{
-    treasury_importer_alloc, treasury_importer_dealloc, treasury_importer_import_trampoline,
-    treasury_importer_name_source_native_trampoline,
-};
-
-goods_treasury_import_ffi::generate_imports_and_exports! {
-    &aseprite::SpriteSheetImporter,
-    &image::ImageImporter,
-    &sprite_sheet::SpriteSheetEnrich,
-    &gltf::GltfObjectImporter,
-}
-
-fn is_default<T: Default + PartialEq>(value: &T) -> bool {
-    *value == T::default()
+treasury_import::make_treasury_importers_library! {
+    [png, jpg, jpeg] qoiconv : image -> qoi = &image::ImageImporter;
 }
