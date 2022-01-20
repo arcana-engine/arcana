5f613a2a35f6c70199fb50d09478ed10d0a3fcbe
Zakarum <zakarumych@ya.ru>
2022-01-21 01:24:24 +0300

    Assets cleanup

--- import/src/image.rs
+++ import/src/image.rs
@@ -1,95 +1,151 @@
+use treasury_import::{Dependencies, ImportError, Sources};
+
 use {
-    eyre::WrapErr,
-    goods_treasury_import::{Importer, Registry},
     std::{io::Read, path::Path},
+    treasury_import::Importer,
 };
 
 pub struct ImageImporter;
 
 impl Importer for ImageImporter {
-    fn name(&self) -> &str {
-        "arcana.image"
-    }
-
-    fn source(&self) -> &str {
-        "image"
-    }
-
-    fn native(&self) -> &str {
-        "rgba.png"
-    }
-
     fn import(
         &self,
         source_path: &Path,
-        native_path: &Path,
-        _registry: &mut dyn Registry,
-    ) -> eyre::Result<()> {
-        let mut source = std::fs::File::open(source_path)
-            .wrap_err_with(|| format!("Failed to open file '{}'", source_path.display()))?;
+        output_path: &Path,
+        _sources: &impl Sources,
+        _dependencies: &impl Dependencies,
+    ) -> Result<(), ImportError> {
+        let mut source = std::fs::File::open(source_path).map_err(|err| ImportError::Other {
+            reason: format!("Failed to open file '{}'. {:#}", source_path.display(), err),
+        })?;
+
+        const QOI_HEADER_SIZE: usize = 14;
 
         // # Find out image format.
         //
         // 1.Read few first bytes.
-        let mut head = [0; 32];
+        let mut head = [0; QOI_HEADER_SIZE];
         let mut read_total = 0;
 
         // This loop is expected to be executed once.
-        while read_total < 32 {
-            let read = source
-                .read(&mut head[read_total..])
-                .wrap_err_with(|| format!("Failed to read file '{}'", source_path.display()))?;
-            if read == 0 {
-                // 1.1 Huh. That's some small image. Try to load it then.
-                image::load_from_memory(&head[..read_total])
-                    .wrap_err_with(|| {
-                        format!("Failed to load image from file '{}'", source_path.display())
-                    })?
-                    .save_with_format(native_path, image::ImageFormat::Png)
-                    .wrap_err_with(|| {
-                        format!("Failed to save image to file '{}'", native_path.display())
-                    })?;
-                return Ok(());
-            }
-            read_total += read;
-        }
+        let image = loop {
+            if read_total < head.len() {
+                let read =
+                    source
+                        .read(&mut head[read_total..])
+                        .map_err(|err| ImportError::Other {
+                            reason: format!(
+                                "Failed to read file '{}'. {:#}",
+                                source_path.display(),
+                                err
+                            ),
+                        })?;
 
-        // 2. Guess format
-        let format = image::guess_format(&head).wrap_err_with(|| {
-            format!(
-                "Failed to guess image format from file '{}'",
-                source_path.display()
-            )
-        })?;
-        match format {
-            image::ImageFormat::Png => {
-                std::fs::copy(source_path, native_path).wrap_err_with(|| {
-                    format!(
-                        "Failed to copy image file '{}' to '{}'",
+                if read == 0 {
+                    // 1.1 Huh. That's some small image. Try to load it then.
+                    break image::load_from_memory(&head[..read_total]).map_err(|err| {
+                        ImportError::Other {
+                            reason: format!(
+                                "Failed to load image from file '{}'. {:#}",
+                                source_path.display(),
+                                err
+                            ),
+                        }
+                    })?;
+                }
+                read_total += read;
+            } else {
+                if rapid_qoi::Qoi::decode_header(&head).is_ok() {
+                    drop(source);
+
+                    std::fs::copy(source_path, output_path).map_err(|err| ImportError::Other {
+                        reason: format!(
+                            "Failed to copy image file '{}' to '{}'. {:#}",
+                            source_path.display(),
+                            output_path.display(),
+                            err,
+                        ),
+                    })?;
+
+                    return Ok(());
+                } else {
+                    let format = image::guess_format(&head).map_err(|err| ImportError::Other {
+                        reason: format!(
+                            "Failed to guess image format from file '{}'. {:#}",
+                            source_path.display(),
+                            err
+                        ),
+                    })?;
+
+                    let mut bytes = vec![];
+                    bytes.extend_from_slice(&head);
+                    source
+                        .read_to_end(&mut bytes)
+                        .map_err(|err| ImportError::Other {
+                            reason: format!(
+                                "Failed to read image file '{}'. {:#}",
+                                source_path.display(),
+                                err,
+                            ),
+                        })?;
+
+                    break image::load_from_memory_with_format(&bytes, format).map_err(|err| {
+                        ImportError::Other {
+                            reason: format!(
+                                "Failed to read image file '{}'. {:#}",
+                                source_path.display(),
+                                err
+                            ),
+                        }
+                    })?;
+                }
+            }
+        };
+
+        match image.color() {
+            image::ColorType::Rgba8 | image::ColorType::Rgba16 | image::ColorType::Bgra8 => {
+                let image = image.into_rgba8();
+
+                let qoi = rapid_qoi::Qoi {
+                    width: image.width(),
+                    height: image.height(),
+                    colors: rapid_qoi::Colors::SrgbLinA,
+                }
+                .encode_alloc(image.as_raw())
+                .map_err(|err| ImportError::Other {
+                    reason: format!("Failed to encode QOI image. {:#}", err),
+                })?;
+
+                std::fs::write(output_path, &qoi).map_err(|err| ImportError::Other {
+                    reason: format!(
+                        "Failed to save image format from file '{}'. {:#}",
                         source_path.display(),
-                        native_path.display()
-                    )
+                        err
+                    ),
                 })?;
-                Ok(())
             }
-            format => {
-                let mut bytes = vec![];
-                bytes.extend_from_slice(&head);
-                source.read_to_end(&mut bytes).wrap_err_with(|| {
-                    format!("Failed to read image file '{}'", source_path.display())
+            _ => {
+                let image = image.into_rgb8();
+
+                let qoi = rapid_qoi::Qoi {
+                    width: image.width(),
+                    height: image.height(),
+                    colors: rapid_qoi::Colors::Srgb,
+                }
+                .encode_alloc(image.as_raw())
+                .map_err(|err| ImportError::Other {
+                    reason: format!("Failed to encode QOI image. {:#}", err),
                 })?;
 
-                let dyn_image =
-                    image::load_from_memory_with_format(&bytes, format).wrap_err_with(|| {
-                        format!("Failed to read image file '{}'", source_path.display())
-                    })?;
-                dyn_image
-                    .save_with_format(native_path, image::ImageFormat::Png)
-                    .wrap_err_with(|| {
-                        format!("Failed to save image file '{}'", native_path.display())
-                    })?;
-                Ok(())
+                std::fs::write(output_path, &qoi).map_err(|err| ImportError::Other {
+                    reason: format!(
+                        "Failed to save image format from file '{}'. {:#}",
+                        source_path.display(),
+                        err
+                    ),
+                })?;
             }
         }
+        Ok(())
     }
 }
